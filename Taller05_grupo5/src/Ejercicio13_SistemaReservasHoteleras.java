324a42443d587f89c79cd7a6435268a067cb4e16
JohanW23M <jawang@utpl.edu.ec>
2024-11-07 20:50:52 -0500

    ejercicio13

--- Taller05_grupo5/src/Ejercicio13_SistemaReservasHoteleras.java
+++ Taller05_grupo5/src/Ejercicio13_SistemaReservasHoteleras.java
@@ -1,12 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author UTPL
- */
+import java.util.Scanner;
 public class Ejercicio13_SistemaReservasHoteleras {
-    
+  
+    public static void main(String[] args) {
+        int dias;
+        double precio;
+        Scanner tlc = new Scanner(System.in);
+        System.out.print("Ingresa los dias que se osperdara el cliente: ");
+        dias = tlc.nextInt();
+        System.out.print("Ingresa el precio de la habitacion por dia: ");
+        precio = tlc.nextDouble();
+        
+        if (dias <= 5) {
+            System.out.println("Valor a pagar: " + ((dias * precio) * 0.90));
+        } 
+        else if (dias < 10) {
+            System.out.println("Valor a pagar: " + ((dias * precio) * 0.90));
+        } 
+        
+        else if (dias >= 10) {
+            System.out.println("Valor a pagar: " + ((dias * precio) * 0.85));
+        }
+        else if (dias < 15){
+            System.out.println("Valor a pagar: " + ((dias * precio) * 0.85));
+        }
+        else if (dias > 15){
+         System.out.println("Valor a pagar: " + ((dias * precio) * 0.80));
+        }
+    }
 }
