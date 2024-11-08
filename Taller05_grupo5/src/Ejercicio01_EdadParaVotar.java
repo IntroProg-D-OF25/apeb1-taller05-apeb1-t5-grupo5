c8f669e3e49749a155175bf0bb1fbb7d816a63f5
erickms5 <erick.emelec.m@gmail.com>
2024-11-07 20:20:20 -0500

    ProyectoLider

---
+++ Taller05_grupo5/src/Ejercicio01_EdadParaVotar.java
@@ -0,0 +1,23 @@
+/**
+ * Edad para Votar: Determinar si una persona es elegible para 
+ * votar en función de su edad (mayor o igual a 18 años).
+ * @author Erick Malla
+ */
+import java.util.Scanner;
+public class Ejercicio01_EdadParaVotar {
+    public static void main(String[] args) {
+        int edad;
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Dame una edad: ");
+        edad = teclado.nextInt();
+        if (edad > 18)
+            System.out.print("Vota");
+        else
+            System.out.print("No vota");
+    }
+}
+/**
+ * run :
+ * Dame una edad: 17
+ * No vota
+ */
---
+++ Taller05_grupo5/src/Ejercicio02_DiaDeLaSemana.java
@@ -0,0 +1,29 @@
+/**
+ * Día de la Semana: Mostrar el nombre del día de la semana en 
+ * función del número ingresado (1 para lunes, 2 para martes, etc.).
+ * @author Erick Malla
+ */
+import java.util.Scanner;
+public class Ejercicio02_DiaDeLaSemana{
+    public static void main(String[] args) {
+     int num_dia;
+     Scanner teclado = new Scanner(System.in);
+     System.out.println("________________________________");
+     System.out.print("Ingrese el numero de dia:  ");
+     num_dia = teclado.nextInt();     
+     System.out.println("--------------------------------");
+     System.out.println("________________________________");
+        switch (num_dia) {
+            case 1 -> System.out.println("Lunes ");
+            case 2 -> System.out.println("Martes ");
+            case 3 -> System.out.println("Miercoles ");
+            case 4 -> System.out.println("Jueves ");
+            case 5 -> System.out.println("Viernes ");
+            case 6 -> System.out.println("Sabado ");
+            case 7 -> System.out.println("Domingo ");
+            default -> System.out.println("ERRROR en el numero de dia  ");
+        }
+     System.out.println("--------------------------------");   
+    }
+}
+
